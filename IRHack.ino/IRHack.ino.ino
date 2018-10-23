265446d515654600263d1a5adae88586a8bafa36
KyrosAnnon <jjpop12345@gmail.com>
2018-10-23 00:27:40 -0500

    Initial commit

--- IRHack.ino/IRHack.ino.ino
+++ IRHack.ino/IRHack.ino.ino
@@ -1,13 +1,7 @@
-const int BLINK_LED = 10;
 void setup() 
 {
-  pinMode(BLINK_LED, OUTPUT);
 }
 
 void loop() 
 {
-  digitalWrite(BLINK_LED, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(500);                       // wait for a second
-  digitalWrite(BLINK_LED, LOW);    // turn the LED off by making the voltage LOW
-  delay(500);                       // wait for a second
 }
